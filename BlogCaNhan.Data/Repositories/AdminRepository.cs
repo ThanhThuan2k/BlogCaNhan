9c56d3a906b6cfc44451dd2bfbbf0e335d6db79c
ThanhThuan2k <thuanhuynh.190800@gmail.com>
2021-01-22 21:58:30 +0700

    Update chức năng thêm mới, danh sách, lock, unlock cho admin

--- BlogCaNhan.Data/Repositories/AdminRepository.cs
+++ BlogCaNhan.Data/Repositories/AdminRepository.cs
@@ -24,5 +24,53 @@ namespace BlogCaNhan.Data.Repositories
                 .OrderByDescending(item => item.NgayTao)
                 .ToPagedList(pageNumber: pageNumber, pageSize: recordPerPage);
         }
+
+        public IPagedList<Admin> AdminBlocked(int pageNumber, int recordPerPage)
+        {
+            return db.Admin.Where(item => item.isBlock == true)
+                .OrderByDescending(item => item.Id)
+                .ToPagedList(pageNumber, recordPerPage);
+        }
+
+        public IPagedList<Admin> SupperAdmin(int pageNumber, int recordPerPage)
+        {
+            return db.Admin.Where(item => item.isSupper == true)
+                .OrderByDescending(item => item.Id)
+                .ToPagedList(pageNumber, recordPerPage);
+        }
+
+        public void ThemMoi(Admin admin)
+        {
+            admin.NgayTao = DateTime.Now;
+            db.Admin.Add(admin);
+            Save();
+        }
+
+        public bool Block(int id = 0)
+        {
+            var result = db.Admin.SingleOrDefault(item => item.Id == id);
+            if(result != null)
+            {
+                result.isBlock = true;
+                Save();
+                return true;
+            }
+            return false;
+        }
+
+        public bool Unlock(int id = 0)
+        {
+            var admin = db.Admin.SingleOrDefault(item => item.Id == id);
+            if(admin != null)
+            {
+                admin.isBlock = false;
+                Save();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
---
+++ BlogCaNhan.Web/wwwroot/js/UploadImage.js
@@ -0,0 +1,34 @@
+﻿const wrapper = document.querySelector(".wrapper");
+const defaultBtn = document.querySelector("#default-btn");
+const customBtn = document.querySelector("#custom-btn");
+const img = document.querySelector("img");
+const cancelBtn = document.querySelector("#cancel-btn");
+const fileName = document.querySelector(".file-name");
+let regExp = /[0-9a-zA-Z\^\&\'\@\{\}\[\]\,\$\=\!\-\#\(\)\.\%\+\~\_ ]+$/;
+
+customBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    defaultBtn.click();
+});
+
+defaultBtn.addEventListener('change', function (e) {
+    const file = this.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            const result = reader.result;
+            img.src = result;
+            wrapper.classList.add("active");
+        }
+        cancelBtn.addEventListener('click', function (e) {
+            img.src = "";
+            wrapper.classList.remove(".active");
+        });
+        reader.readAsDataURL(file);
+    }
+    if (this.value) {
+        let valueStore = this.value.match(regExp);
+        fileName.textContent = valueStore;
+    }
+});
